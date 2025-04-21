47c13dd0f45ca91d0536883bb20e24256a67ea72
Vk9898 <viktor@found.xyz>
2025-04-21 09:33:35 -0500

    ffff

--- src/features/globals/providers.tsx
+++ src/features/globals/providers.tsx
@@ -1,10 +1,25 @@
 "use client";
 import { SessionProvider } from "next-auth/react";
+import { ThemeProvider } from "@/features/theme/theme-provider";
+import { Toaster } from "@/components/ui/sonner";
+import { auth } from "@/features/auth-page/auth-api";
 
 export const AuthenticatedProviders = ({
   children,
 }: {
   children: React.ReactNode;
 }) => {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SessionProvider>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="dark"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+        <Toaster />
+      </ThemeProvider>
+    </SessionProvider>
+  );
 };
--- src/features/theme/theme-provider.tsx
+++ src/features/theme/theme-provider.tsx
@@ -1,8 +1,17 @@
 "use client";
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { type ThemeProviderProps } from "next-themes/dist/types";
+import * as React from "react";
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+
+export function ThemeProvider({ 
+  children, 
+  ...props 
+}: React.PropsWithChildren<{
+  attribute?: "class" | "data-theme";
+  defaultTheme?: string;
+  enableSystem?: boolean;
+  disableTransitionOnChange?: boolean;
+}>) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
